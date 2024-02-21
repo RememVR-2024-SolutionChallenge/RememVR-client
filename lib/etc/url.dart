f700642340a27536d32e4ae1f62430c254d70b1d
ryan00102 <101000358+ryan00102@users.noreply.github.com>
2024-02-22 03:34:52 +0900

    Fix: fix url

--- lib/etc/url.dart
+++ lib/etc/url.dart
@@ -2,7 +2,7 @@ String baseUrl = "https://gateway-server-n3wk2vhygq-uc.a.run.app";
 String giverVrUrl =
     'https://rememvr-2024-solutionchallenge.github.io/vr-viewer-test';
 String recipientVrUrl =
-    'https://rememvr-2024-solutionchallenge.github.io/vr-viewer-test';
+    'https://rememvr-2024-solutionchallenge.github.io/vr-viewer-test/viewer';
 Map<String, String> badgeTypes = {
   "GREAT_JOB": "badge1.png",
   "EXCELLENT_WORK": "badge2.png",
